640fbffde61f0d35f62fb0c7e4462c26efcb18ad
Jeffrey Allen <jeffreyallen92@gmail.com>
2018-01-08 13:46:15 -0600

    Added first controller, route, and response object

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeffreyallen
+ * Date: 1/8/18
+ * Time: 1:38 PM
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky numer:'. $number .'</body></html>'
+        );
+    }
+
+}
